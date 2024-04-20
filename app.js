2b62092185527924854c9db819b27e47b4727dc4
Elisaudo <elisaudos@gmail.com>
2024-04-20 11:57:44 -0300

    Alteração package.json

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+const mongoose = require('mongoose');
 const express = require('express');
-const connectDB = require('./config/db');
+//const connectDB = require('./config/db');
 const booksRoutes = require('./routes/api/books');
 const cors = require('cors');
 require("dotenv").config();
@@ -18,7 +19,25 @@ app.use(express.json({ extended: false }));
 //app.get('/', (req, res) => res.send('Hello world!'));
 
 //Conecta o Banco de Dados
+const uri = process.env.MONGODB_URI;
+
+const connectDB = async () => {
+    try {
+        mongoose.set('strictQuery', true);
+        await mongoose.connect(uri, {
+            useNewUrlParser:true,
+        });
+        console.log('MongoDB está conectado...');
+    } catch (err){
+        console.error(err.message);
+        process.exit(1)
+    }
+};
+
 connectDB();
+
+
+
 
 // Cors
 app.use(cors({ origin: true, credentials: true }));
